9b8e4ad83403640964b440fbdebd341fb243f59e
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-12-15 16:30:43 -0300

    feat: provê listagem de cursos

--- src/pages/Curso/Curso.jsx
+++ src/pages/Curso/Curso.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CriarCursoModal } from "@comp/form/CriarCurso";
 import { Menu } from "@comp/ui/Menu";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
+import supabase from "@api/supabase.js";
 
 const Curso = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [cursos, setCursos] = useState([]);
+
+  async function getData() {
+    let { data: curso, error } = await supabase.from("curso").select("*");
+    if (!error) {
+      setCursos(curso);
+    } else {
+      alert("Erro ao buscar os cursos");
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <Grid component="main">
@@ -15,12 +31,13 @@ const Curso = () => {
       <Box>
         <h1>Curso</h1>
         <button onClick={() => setIsModalOpen(true)}>Criar curso</button>
-      <CriarCursoModal show={isModalOpen} handleClose={() => setIsModalOpen(false)} />
+        <CriarCursoModal
+          show={isModalOpen}
+          handleClose={() => setIsModalOpen(false)}
+        />
       </Box>
     </Grid>
   );
 };
 
 export default Curso;
-
-
