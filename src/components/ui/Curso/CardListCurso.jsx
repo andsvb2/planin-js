cdb90c8657c2d647e81e0312d11fda7922ca9cbd
Karoline Diniz Ramos <dinizkaroline923@gmail.com>
2023-12-15 18:57:50 -0300

    refactor: estiliza card e tela de curso

--- src/components/ui/Curso/CardListCurso.jsx
+++ src/components/ui/Curso/CardListCurso.jsx
@@ -5,16 +5,16 @@ import Grid from "@mui/material/Grid";
 
 export default function CardListCurso({ instituicao_campus, nome_curso }) {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275, backgroundColor: "#F2EFEF", ml:3 , mr:3, mb: 3 }}>
       <CardContent>
         <Grid container direction="row" alignItems="center">
           <Grid item xs={4}>
-            <Typography variant="h5" align="center">
+            <Typography variant="body1" align="center">
               {instituicao_campus}
             </Typography>
           </Grid>
           <Grid item>
-            <Typography variant="body2" textAlign="center">
+            <Typography variant="h6" textAlign="center">
               {nome_curso}
             </Typography>
           </Grid>
