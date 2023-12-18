ddc7e397d78dd94ba58795acff37590c12534b7c
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-12-18 01:54:30 -0300

    chore: cria componente AutocompleteRHF

---
+++ src/components/form/AutocompleteRHF/AutocompleteRHF.jsx
@@ -0,0 +1,47 @@
+import { useController } from "react-hook-form";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const AutocompleteRHF = ({ defaultProps, control, name, rules, ...rest }) => {
+  const {
+    field: { onChange, value, ref },
+    fieldState: { invalid, isTouched, isDirty },
+    formState: { touchedFields, dirtyFields },
+  } = useController({
+    name,
+    control,
+    rules: { ...rules },
+  });
+
+  const id = rest.id ? rest.id : name;
+  const labelText = rest.tag ? rest.tag : rest.label;
+
+  return (
+    <Stack gap={3} direction="column">
+      <InputLabel htmlFor={id} variant="outlined">
+        {labelText}
+      </InputLabel>
+
+      <Autocomplete
+        {...defaultProps}
+        name={name}
+        onChange={(event, newValue) => {
+          onChange(newValue ? newValue.id : null);
+        }}
+        control={control}
+        renderInput={(params) => (
+          <TextField
+            {...rest}
+            {...params}
+            helperText={rest.helperText}
+            inputRef={ref}
+          />
+        )}
+      />
+    </Stack>
+  );
+};
+
+export default AutocompleteRHF;
