7a42b5d6853922ca887156e70dc43b3fc60c18ce
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-12-17 22:37:47 -0300

    chore: limpa teste

--- src/pages/Teste.jsx
+++ src/pages/Teste.jsx
@@ -1,28 +1,7 @@
-import supabase from "@services/supabase.js";
-import { useEffect } from "react";
-
 const Teste = () => {
-  const GOOGLE_DATA_CLIENT_ID = import.meta.env.VITE_GOOGLE_DATA_CLIENT_ID;
-  console.log(GOOGLE_DATA_CLIENT_ID);
-
-  function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-  }
-  window.onload = function () {
-    google.accounts.id.initialize({
-      client_id: GOOGLE_DATA_CLIENT_ID,
-      callback: handleCredentialResponse,
-    });
-    google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large" }, // customization attributes
-    );
-    google.accounts.id.prompt(); // also display the One Tap dialog
-  };
-
   return (
     <>
-      <div id="buttonDiv"></div>
+      <h1>oi</h1>
     </>
   );
 };
